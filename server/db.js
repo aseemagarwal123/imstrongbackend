bd34c035b8a2330a665c4dc8d8ce026380d1bf92
aseemagarwal123 <35007308+aseemagarwal123@users.noreply.github.com>
2019-11-27 04:54:32 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 // db.js
 
 const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient('mongodb+srv://aseem:aseem123@wtf-m8db4.mongodb.net/test?retryWrites=true&w=majority')
+const client = new MongoClient(process.env.DATABASE_URL)
 
 client.connect(function(err) {
 console.log("Connected successfully to db");
